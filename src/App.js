b19f2b892711758f24f9d7a812c6e578419b1e37
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2022-12-06 12:34:01 +0500

    zipcodes game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { MapContainer,GeoJSON} from 'react-leaflet';
 import './App.css';
+import bar from "./zonesdata.json";
+import 'leaflet/dist/leaflet.css';
+import * as L from 'leaflet'
+// import ReactDOMServer from "react-dom/server";
+
+const defaultCenter = [25.740529092773226,73.30627441406251];
+const defaultZoom = 8;
 
 function App() {
+  const mapRef = useRef();
+  function onEachFeature(feature, layer) {
+    if (feature.properties) {
+      layer.on('click',(e)=>{
+        let code=feature.properties.Zipcode;
+        layer.bindPopup(`Zip Code : ${code}`).openPopup();
+      })
+    }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer ref={mapRef} center={defaultCenter} zoom={defaultZoom}>
+        {/* <TileLayer url={gameTiles}
+        attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" /> */}
+        <GeoJSON data={bar}  onEachFeature={onEachFeature} />
+      </MapContainer>
     </div>
   );
 }
